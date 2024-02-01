8031627ca340fb20cd4bca4302e8f8c3a39064af
Vlad A. <Centr575@yandex.ru>
2024-02-01 19:43:33 +0300

    update

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,16 @@
 from prompt import string
 
 
+def welcome_name():
+    print("Welcome to the Brain Games!")
+    name = string("May I have your name? ")
+    print("Hello, " + name + "!")
+    return name
+    
+
+
 def welcome_user():
     name = string("May I have your name? ")
     return print("Hello, " + name + "!")
 
+#welcome_name()
