27a74f984563561d05b06ebf741fac69b00990e6
Vlad A. <Centr575@yandex.ru>
2024-02-06 18:09:15 +0300

    add script brain-even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,10 @@
+from brain_games.logic_games import logic
+from brain_games.games.even import game_even, exercise_even
+
+
+def main():
+    logic(game_even, exercise_even)
+
+
+if __name__ == '__main__':
+    main()
