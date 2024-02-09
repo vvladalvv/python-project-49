44c874a230e9fbdc4250ce06e5219efc1b99260c
Vlad A. <Centr575@yandex.ru>
2024-02-09 16:57:12 +0300

    update even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,38 +4,20 @@ from brain_games.constants import ANSWER_YES, ANSWER_NO
 EXERCISE_EVEN = "Answer 'yes' if the number is even, otherwise answer 'no'."
 
 
-def check_even(num):
-    if num % 2 == 0:
-        return True
-    else:
-        return False
-
 def game_even():
     random_number = randint(1, 101)
-    result = check_even(random_number)
     print(f"Question: {random_number}")
     answer = input('Your answer: ').lower()
-    if result is True and answer == 'yes':
+    if random_number % 2 == 0 and answer == ANSWER_YES:
         return True
-    elif result is not True and answer == 'no':
+    elif random_number % 2 != 0 and answer == ANSWER_NO:
         return True
     else:
-        if answer == 'yes':
-            print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'")
+        if answer == ANSWER_YES:
+            print(f"'{answer}' {INCORRECT_ANSWER} '{ANSWER_NO}'")
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'")
+            print(f"'{answer}' {INCORRECT_ANSWER} '{ANSWER_YES}'")
         return False
-    
-    #if random_number % 2 == 0 and answer == ANSWER_YES:
-        #return True
-    #elif random_number % 2 != 0 and answer == ANSWER_NO:
-        #return True
-    #else:
-        #if answer == ANSWER_YES:
-            #print(f"'{answer}' {INCORRECT_ANSWER} '{ANSWER_NO}'")
-        #else:
-            #print(f"'{answer}' {INCORRECT_ANSWER} '{ANSWER_YES}'")
-        #return False
 
 
 # game_even()
