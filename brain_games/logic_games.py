8601710cff32fd6306a2aa4177e788e3fcdc22fa
Vlad A. <Centr575@yandex.ru>
2024-02-14 17:22:41 +0300

    fix code 2

--- brain_games/logic_games.py
+++ brain_games/logic_games.py
@@ -3,7 +3,7 @@ from brain_games.cli import welcome_user
 # from brain_games.games.progression import find_progression, DESCRIPTION_PROGRESS
 # from brain_games.games.prime import find_prime_number, DESCRIPTION_PRIME
 # from brain_games.games.calc import get_question_and_answer, DESCRIPTION_CALC
-from brain_games.games.even import find_even_number, DESCRIPTION_EVEN
+# from brain_games.games.even import find_even_number, DESCRIPTION_EVEN
 
 
 INCORRECT_ANSWER = 'is wrong answer ;(. Correct answer was'
@@ -33,14 +33,14 @@ def run_game(game_name, description):
             correct_round += counter_correct
         else:
             correct_round = 0
-            print(f"'{user_answer}' {INCORRECT_ANSWER}, '{correct_answer}'")
+            print(f"'{user_answer}' {INCORRECT_ANSWER}, '{correct_answer}'.")
             print(f"{MESSAGE_REPEAT}, {name}!")
             return
     return print(f"{CONGRATULATIONS}, {name}!")
 
 
 # run_game(get_question_and_answer, DESCRIPTION_CALC)
-run_game(find_even_number, DESCRIPTION_EVEN)
+# run_game(find_even_number, DESCRIPTION_EVEN)
 # run_game(find_gcd, DESCRIPTION_GCD)
 # run_game(find_progression, DESCRIPTION_PROGRESS)
 # run_game(find_prime_number, DESCRIPTION_PRIME)
